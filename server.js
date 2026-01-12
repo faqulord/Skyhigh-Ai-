1033a6b410f4f46fa90bf97f7ce58c1d4104fbeb
faqulord <stylefaqu@gmail.com>
2026-01-12 19:50:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080; // Railway-hez igazítva
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// Útvonalak (Routes)
+app.get('/', (req, res) => { res.render('index'); });
+app.get('/technologia', (req, res) => { res.render('tech'); });
+app.get('/strategia', (req, res) => { res.render('profit'); });
+app.get('/mlm', (req, res) => { res.render('mlm'); });
+app.get('/regisztracio', (req, res) => { res.render('register'); });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Skyhigh.Ai élesítve a ${PORT} porton!`);
+    console.log(`Skyhigh AI fut a ${PORT} porton`);
 });
