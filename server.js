c8bf5cc64b2db7f59de2cc21a796cb6c2bd629d2
faqulord <stylefaqu@gmail.com>
2026-01-12 19:56:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// Útvonalak (Routes)
 app.get('/', (req, res) => { res.render('index'); });
 app.get('/technologia', (req, res) => { res.render('tech'); });
 app.get('/strategia', (req, res) => { res.render('profit'); });
@@ -13,6 +11,4 @@ app.get('/mlm', (req, res) => { res.render('mlm'); });
 app.get('/regisztracio', (req, res) => { res.render('register'); });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Skyhigh AI fut a ${PORT} porton`);
-});
+app.listen(PORT, () => { console.log('Skyhigh AI Online'); });
