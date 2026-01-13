1d067fb0b3f033a611ac9ca1c87c5771e4595e7a
faqulord <stylefaqu@gmail.com>
2026-01-13 01:19:30 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,19 +4,12 @@ const UserSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    date: { type: Date, default: Date.now },
-    
-    // LICENC ADATOK
+    isAdmin: { type: Boolean, default: false },
     hasLicense: { type: Boolean, default: false },
-    licenseType: { type: String, default: 'none' },
     licenseExpires: { type: Date, default: null },
-    totalSpent: { type: Number, default: 0 },
-    
-    // PÉNZÜGYI MENEDZSMENT (ROBOT KEZELI)
-    startingCapital: { type: Number, default: 0 }, // Kezdőtőke
-    currentCapital: { type: Number, default: 0 },  // Aktuális egyenleg
-    
-    myReferralCode: { type: String, default: '' }
+    startingCapital: { type: Number, default: 0 },
+    currentCapital: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', UserSchema);
